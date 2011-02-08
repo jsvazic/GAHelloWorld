f7d37733015365bdde73f6ca01779ae193c7965f
John Svazic <jsvazic@gmail.com>
2011-02-08 08:07:30 -0500

    Added a :main attribute for the lein build script to generate an uberjar.

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,5 @@
 (defproject gahelloworld "1.0.0-SNAPSHOT"
   :description "Clojure implementation of a \"Hello, world!\"  genetic algorithm simulation"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :main gahelloworld.core)
