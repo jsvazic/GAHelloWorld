5f73f8a39cc2536e6a25363a557470f5673e818d
Victor Nicollet <victor-github@nicollet.net>
2011-04-16 13:46:55 +0200

    Adding OCaml version

---
+++ ocaml/README.md
@@ -0,0 +1,33 @@
+# Genetic Algorithm Hello World! (OCaml Edition)
+
+This is a simple project intended to showcase genetic algorithms with a well 
+known example for all new developers; namely the classic "Hello, world!" 
+example, written in OCaml.
+
+## Overview
+
+The application simply "evolves" the string "Hello, world!" from a population 
+of random strings.  It is intended to be a gentle introduction into the world
+of genetic algorithms, specifically with OCaml 3.12.  The program itself is 
+really simple, and is contained within a single file.
+
+The "GENOME" and "Algorithm" primitives can be reused to run genetic
+algorithms on other problems and genome types quite easily. Check the 
+ocamldoc documentation inside for more details.
+
+## Usage
+
+> ocaml gahelloworld.ml
+
+## Copyright and License
+
+The MIT License
+
+Copyright © 2011 Victor Nicollet
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ ocaml/gahelloworld.ml
@@ -0,0 +1,202 @@
+(** Genetic algorithms.
+
+    This module defines a {!GENOME} module type to represent arbitrary genomes,
+    an {!Algorithm} functor which allows running a genetic algorithm on that
+    genome, and a {!LazyFitness} utility functor for allowing memoization of
+    the fitness function.
+
+    @author Victor Nicollet
+*)
+
+(* Copyright (C) 2011 by Victor Nicollet
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+   
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*)
+
+(** A genome, represented as a generic module type. *)
+module type GENOME = sig
+
+  (** What a genome is. Expected to be immutable. *)
+  type t 
+
+  (** How to combine two genomes as a crossover *)
+  val mate : t -> t -> t
+
+  (** Mutate a single gene in a genome. *)
+  val mutate : t -> t
+
+  (** Create a random genome. *)
+  val random : unit -> t
+
+  (** The fitness of a genome. The smaller, the better. Zero means success. *)
+  val fitness : t -> int
+
+end
+
+(** A lazy-evaluation wrapper for genomes: this prevents the fitness
+    from being re-computed on every call. 
+*)
+module LazyFitness = functor(Genome:GENOME) -> ( struct
+    
+  type t = { genome : Genome.t ; fitness : int Lazy.t }
+
+  let wrap inner = { genome = inner ; fitness = lazy (Genome.fitness inner) }
+
+  let mate g1 g2 = wrap (Genome.mate g1.genome g2.genome)
+
+  let mutate g = wrap (Genome.mutate g.genome)
+
+  let random () = wrap (Genome.random ())
+
+  let fitness g = Lazy.force g.fitness
+
+  let value g = g.genome
+
+end : sig
+
+  include GENOME
+
+  (** Return the wrapped value. *)
+  val value : t -> Genome.t
+
+end )
+
+(** Utility function for comparing two values by applying a function *)
+let by f a b = compare (f a) (f b)
+
+(** Running an algorithm on a given kind genome. *)
+module Algorithm = functor(Genome:GENOME) -> struct      
+
+  (** Run the algorithm. Returns the list of the best elements in each generation.
+      @param tsize Tournament size 
+      @param psize Population size
+      @param crossover Crossover probability (0-1)
+      @param elitism The top elitism% of the population is kept on each generation.
+      @param mutation Mutation probability (0-1)
+      @param generations Maximum number of generations allowed
+  *)
+  let run ~tsize ~psize ~crossover ~elitism ~mutation ~generations () =     
+
+    let elitism_bound = int_of_float (float_of_int psize *. elitism) in
+
+    (* Select a random subset of the population, then keep the genome
+       with the highest fitness in that subset. [tsize] is the size of
+       the tournament. This function relies on the fact that the
+       smaller the index of a genome in the population, the better
+       its fitness.
+    *)
+    let tselect pop = 
+      let idx = Array.init (tsize-1) (fun _ -> Random.int psize) in
+      pop.(Array.fold_left min (Random.int psize) idx)
+    in
+
+    (* Initial population based on random genomes *)
+    let initial = Array.init psize (fun _ -> Genome.random ()) in
+    Array.sort (by Genome.fitness) initial ;
+
+    (* Evolve a new population based on the previous one *)
+    let evolve pop = 
+      
+      let pop' = Array.init psize begin fun i -> 
+
+	  let gene = 
+	    if i >= elitism_bound && Random.float 1.0 <= crossover
+	    then Genome.mate (tselect pop) (tselect pop)
+	    else pop.(i)
+	  in
+
+	  if i >= elitism_bound && Random.float 1.0 <= mutation
+	  then Genome.mutate gene
+	  else gene
+
+      end in
+
+      Array.sort (by Genome.fitness) pop' ; pop'
+
+    in
+
+    (* Loop until we run out of generations or the best genome in the population
+       has solved the task. Return the best element of each generation.
+    *)
+    let rec loop generations pop = 
+      if generations = 0 then [] else 
+	if Genome.fitness pop.(0) = 0 then [pop.(0)] else
+	  pop.(0) :: loop (generations - 1) (evolve pop)
+    in
+
+    loop generations initial
+
+end
+
+(**/**)
+
+(* Running our hello world algorithm ------------------------------------------- *)
+
+(* "Hello World" genomes are strings of the same length as the target. 
+   
+   While not immutable (because they are strings), genomes are never 
+   modified by the functions inside.
+*)
+module HelloWorld = struct
+
+  type t = string
+
+  let target = "Hello, world!"
+  let length = String.length target
+
+  let randchar () = Char.chr (Random.int (126-32) + 32)
+
+  let mate g1 g2 = 
+    let n = Random.int length in
+    String.sub g1 0 n ^ String.sub g2 n (length - n)
+
+  let mutate g = 
+    (* Create a copy to avoid mutation. *)
+    let g' = String.copy g in
+    g'.[Random.int length] <- randchar () ; g'
+
+  let random () = 
+    let g = String.create length in
+    for i = 0 to length - 1 do 
+      g.[i] <- randchar ()
+    done ; g
+
+  (* Fitness is the number of characters that do not match the target. *)
+  let fitness g = 
+    let d = ref 0 in
+    for i = 0 to length - 1 do
+      if g.[i] <> target.[i] then incr d
+    done ; !d
+
+end
+
+module MyGenome     = LazyFitness(HelloWorld)
+module MyAlgorithm  = Algorithm(MyGenome)
+
+let algorithm = MyAlgorithm.run 
+  ~tsize:3
+  ~psize:2048
+  ~crossover:0.8
+  ~elitism:0.1
+  ~mutation:0.3
+  ~generations:16384
+
+let results = 
+  List.iter print_endline
+    (List.map MyGenome.value (algorithm ()))
