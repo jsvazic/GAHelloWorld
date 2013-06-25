34b0f5c30159bdc1c4e85d711116a781a496495c
Song Gao <song@gao.io>
2013-06-24 23:22:34 -0700

    moved fitness function in main.go from literal to a defined function, making it easier for test

--- go/main.go
+++ go/main.go
@@ -8,6 +8,25 @@ import (
 
 var objective = []byte("Hello, World!")
 
+// Fitness function should be designed to work with the algorithm. In this
+// example, apparently crossover/mutation does not involve anything like
+// generating a new byte based on existing bytes. All it does is to either pick
+// current bytes from a parent, or randomly generate a new one. For this
+// particular algorithm, ascii value difference doesn't matter, i.e. there's no
+// difference between "difference of a and z" and "differenceo of a and b".
+// Thus, the evaluation function should not make a difference on that.
+// Otherwise, "Gckkp, Xpskc!" will rank better than "Hello, Worzz!", which
+// leads the evolving to a wrong direction.
+func evaluate(gene []byte) float64 {
+	var fitness float64
+	for i, c := range gene {
+		if objective[i] != c {
+			fitness += 1
+		}
+	}
+	return fitness
+}
+
 func main() {
 
 	rand.Seed(time.Now().Unix())
@@ -20,24 +39,7 @@ func main() {
 	param.MutationProbability = .2
 	param.PopulationSize = 100
 
-	// Fitness function should be designed to work with the algorithm. In this
-	// example, apparently crossover/mutation does not involve anything like
-	// generating a new byte based on existing bytes. All it does is to either
-	// pick current bytes from a parent, or randomly generate a new one. For this
-	// particular algorithm, ascii value difference doesn't matter, i.e. there's
-	// no difference between "difference of a and z" and "differenceo of a and
-	// b".  Thus, the evaluation function should not make a difference on that.
-	// Otherwise, "Gckkp, Xpskc!" will rank better than "Hello, Worzz!", which
-	// leads the evolving to a wrong direction.
-	param.Evaluator = func(gene []byte) float64 {
-		var fitness float64
-		for i, c := range gene {
-			if objective[i] != c {
-				fitness += 1
-			}
-		}
-		return fitness
-	}
+	param.Evaluator = evaluate
 
 	for run := 0; run < 8; run++ { // run the same algorithm 8 times
 		start := time.Now()
